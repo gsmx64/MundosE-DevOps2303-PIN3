7c645fcfad05998325e6c302fef3d59aa7bcf952
Gonzalo Mahserdjian <gsmx64@outlook.com>
2024-02-23 23:11:44 -0300

    Split ec2 bash script files

--- 01_ec2/ec2_user_data_07_terraform.sh
+++ 01_ec2/ec2_user_data_07_terraform.sh
@@ -3,4 +3,5 @@
 sudo yum install -y yum-utils shadow-utils
 sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
 sudo yum install -y terraform
+terraform --version
 sleep 4
