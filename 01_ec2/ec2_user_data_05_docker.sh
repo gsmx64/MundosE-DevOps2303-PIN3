a2c3fbdb5ad4f5d0d80021a836452a69998b873d
Gonzalo Mahserdjian <gsmx64@outlook.com>
2024-02-24 01:51:20 -0300

    Testing

--- 01_ec2/ec2_user_data_05_docker.sh
+++ 01_ec2/ec2_user_data_05_docker.sh
@@ -4,23 +4,25 @@
 source $PWD/.env
 
 echo " > Installing docker."
-sudo yum install -y docker
+yum install -y docker
 echo " > Creating the docker user and group."
-sudo usermod -a -G docker $LOCAL_USER
+usermod -a -G docker $LOCAL_USER
 newgrp docker
+echo " > Configuring the Docker daemon to enable state and start on boot."
+systemctl enable docker.service
+systemctl start docker.service
 echo " > Verifying that the $LOCAL_USER can run Docker commands without using sudo."
 docker ps
 sleep 4
-echo " > Downloading the docker-compose binary."
-wget https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) 
-echo " > Applying execute permissions to the binary."
-sudo mv docker-compose-$(uname -s)-$(uname -m) /usr/local/bin/docker-compose
-echo " > Applying execute permissions to the binary."
-sudo chmod -v +x /usr/local/bin/docker-compose
-echo " > Configuring the Docker daemon to enable state and start on boot."
-sudo systemctl enable docker.service
-sudo systemctl start docker.service
-echo " > Verifying the docker and docker-compose version."
+echo " > Verifying the docker version."
 docker --version
+sleep 4
+echo " > Downloading the docker-compose binary."
+wget https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)
+echo " > Applying execute permissions to the binary."
+mv docker-compose-$(uname -s)-$(uname -m) /usr/local/bin/docker-compose
+echo " > Applying execute permissions to the binary."
+chmod -v +x /usr/local/bin/docker-compose
+echo " > Verifying the docker-compose version."
 docker-compose --version
 sleep 4
