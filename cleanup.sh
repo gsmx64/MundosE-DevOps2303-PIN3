979babad520e84cc98d0eaf32b3b90a85c9cbc83
Gonzalo Mahserdjian <gsmx64@outlook.com>
2024-02-25 02:55:14 -0300

    Testing

--- cleanup.sh
+++ cleanup.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
 echo "-------------------------------------------------"
-echo " > Installing EBS Driver"
+echo " > Cleaning up EKS Cluster"
 echo "-------------------------------------------------"
 echo " "
-$PWD/03_monitoring/ebsdriver-deploy.sh
-sleep 4
-
 
 echo " > Uninstalling Prometheus"
 sudo helm uninstall prometheus --namespace prometheus kubectl delete ns prometheus
 
-echo " > Unnstalling Grafana"
+echo " > Uninstalling Grafana"
 sudo helm uninstall grafana --namespace grafana kubectl delete ns grafana
 rm -rf ${HOME}/environment/grafana
 sleep 4
 
-echo " > Unnstalling EKS"
+echo " > Uninstalling EKS"
 eksctl delete cluster --name eks-mun
+
+echo " "
+echo "-------------------------------------------------"
+echo " > Cleaning up finished."
+echo "-------------------------------------------------"
+echo " "
